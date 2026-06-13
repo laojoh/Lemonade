47ce9d97f5bc7254de6f6d8150dcc0d0b482eb6a
laojoh <johne12lao@gmail.com>
2026-06-12 23:46:50 -0700

    More game progress, added dashing and wall sliding also limited jumps and particles!

--- assets/Game.py
+++ assets/Game.py
@@ -1,9 +1,13 @@
+import math
+
 import pygame
 import sys
+import random
 from scripts.entities import PhysicsEntity, Player
 from scripts.utils import Animation, load_image, load_images
 from scripts.tilemap import Tilemap
 from scripts.clouds import Clouds
+from scripts.particle import Particle
 
 class Game:
     def __init__(self):
@@ -29,10 +33,9 @@ class Game:
             "player/run" : Animation(load_images("/entities/player/run"), img_dur = 4),
             "player/jump" : Animation(load_images("/entities/player/jump")),
             "player/slide" : Animation(load_images("/entities/player/slide")),
-            "player/wall_slide" : Animation(load_images("/entities/player/wall_slide"))
-
-
-
+            "player/wall_slide" : Animation(load_images("/entities/player/wall_slide")),
+            "particle/leaf" : Animation(load_images("/particles/leaf"), img_dur = 15, loop = False),
+            "particle/particle" : Animation(load_images("/particles/particle"), img_dur = 6, loop = False),
         }
 
         self.clouds = Clouds(self.assets["clouds"], count = 16)
@@ -40,6 +43,14 @@ class Game:
         self.player = Player(self, (50, 50), (8, 15))
 
         self.tilemap = Tilemap(self, tile_size = 16)
+        self.tilemap.load("map.json")
+
+        self.leaf_spawners = []
+        for tree in self.tilemap.extract([("large_decor", 2)], keep = True):
+            self.leaf_spawners.append(pygame.Rect(4 + tree["pos"][0], 4 + tree["pos"][1], 23, 13))
+
+        self.particles = []
+
 
         self.scroll = [0, 0]
 
@@ -51,6 +62,12 @@ class Game:
             self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
             render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
 
+            for rect in self.leaf_spawners:
+                if random.random() * 49999 < rect.width * rect.height:
+                    pos = (rect.x + random.random() * rect.width, rect.y + random.random() * rect.height)
+                    self.particles.append(Particle(self, "leaf", pos, velocity = [random.random() * 0.5 - 0.25, random.random() * 0.2 + 0.25], frame = random.randint(0, 20)))
+
+
             self.clouds.update()
             self.clouds.render(self.display, offset = render_scroll)
 
@@ -58,6 +75,14 @@ class Game:
 
             self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
             self.player.render(self.display, offset = render_scroll)
+
+            for particle in self.particles.copy():
+                kill = particle.update()
+                particle.render(self.display, offset = render_scroll)
+                if particle.type == "leaf":
+                    particle.pos[0] += math.sin(particle.animation.frame * 0.035) * 0.3
+                if kill:
+                    self.particles.remove(particle)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -70,8 +95,9 @@ class Game:
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
                     if event.key == pygame.K_UP:
-                        self.player.velocity[1] = -3
-
+                        self.player.jump()
+                    if event.key == pygame.K_x:
+                        self.player.dash()
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
                         self.movement[0] = False
--- assets/scripts/editor.py
+++ assets/scripts/editor.py
@@ -10,7 +10,7 @@ class Editor:
         pygame.init()
 
         pygame.display.set_caption("editor")
-        self.screen = pygame.display.set_mode((480, 320))
+        self.screen = pygame.display.set_mode((640, 480))
         self.display = pygame.Surface((320, 240))
 
         self.clock = pygame.time.Clock()
@@ -26,6 +26,11 @@ class Editor:
         self.movement = [False, False, False, False]
 
         self.tilemap = Tilemap(self, tile_size = 16)
+
+        try:
+            self.tilemap.load("map.json")
+        except FileNotFoundError:
+            pass
 
         self.scroll = [0, 0]
 
@@ -53,16 +58,26 @@ class Editor:
 
             mpos = pygame.mouse.get_pos()
             mpos = (mpos[0] / RENDER_SCALE, mpos[1] / RENDER_SCALE)
-            tile_pos = (int((mpos[0] + self.scroll[0]) // self. tilemap.tile_size), int((mpos[1] + self.scroll[1]) // self.tilemap.tile_size))
+            tile_pos = (int((mpos[0] + self.scroll[0]) // self.tilemap.tile_size), int((mpos[1] + self.scroll[1]) // self.tilemap.tile_size))
 
-            self.display.blit(current_tile_img, (tile_pos[0] * self.tilemap.tile_size - self.scroll[0], tile_pos[1] * self.tilemap.tile_size - self.scroll[1]))
 
-            if self.clicking:
+            if self.ongrid:
+                self.display.blit(current_tile_img, (tile_pos[0] * self.tilemap.tile_size - self.scroll[0], tile_pos[1] * self.tilemap.tile_size - self.scroll[1]))
+            else: 
+                self.display.blit(current_tile_img, mpos)
+
+
+            if self.clicking and self.ongrid:
                 self.tilemap.tilemap[str(tile_pos[0]) + ";" + str(tile_pos[1])] = {"type" : self.tile_list[self.tile_group], "variant" : self.tile_variant, "pos" : tile_pos}
             if self.right_clicking:
                 tile_loc = str(tile_pos[0]) + ";" + str(tile_pos[1])
                 if tile_loc in self.tilemap.tilemap:
                     del self.tilemap.tilemap[tile_loc]
+                for tile in self.tilemap.offgrid_tiles.copy():
+                    tile_img = self.assets[tile["type"]][tile["variant"]]
+                    tile_r = pygame.Rect(tile["pos"][0] - self.scroll[0], tile["pos"][1] - self.scroll[1], tile_img.get_width(), tile_img.get_height())
+                    if tile_r.collidepoint(mpos):
+                        self.tilemap.offgrid_tiles.remove(tile)
 
             self.display.blit(current_tile_img, (5, 5))
 
@@ -74,6 +89,8 @@ class Editor:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 1:
                         self.clicking = True
+                        if not self.ongrid:
+                            self.tilemap.offgrid_tiles.append({"type" : self.tile_list[self.tile_group], "variant" : self.tile_variant, "pos" : (mpos[0] + self.scroll[0], mpos[1] + self.scroll[1])})
                     if event.button == 3:
                         self.right_clicking = True
                     if self.shift:
@@ -93,7 +110,7 @@ class Editor:
                     if event.button == 1:
                         self.clicking = False
                     if event.button == 3:
-                        self.clicking = False
+                        self.right_clicking = False
 
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_a:
@@ -106,6 +123,10 @@ class Editor:
                         self.movement[3] = True
                     if event.key == pygame.K_g:
                         self.ongrid = not self.ongrid
+                    if event.key == pygame.K_t:
+                        self.tilemap.autotile()
+                    if event.key == pygame.K_o:
+                        self.tilemap.save("map.json")
                     if event.key == pygame.K_LSHIFT:
                         self.shift = True
 
@@ -121,7 +142,7 @@ class Editor:
                     if event.key == pygame.K_LSHIFT:
                         self.shift = False
             
-            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()))
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
             pygame.display.update()
             self.clock.tick(60)
 
