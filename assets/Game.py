843818b78fdc20f327359e478acc17ae073a2152
laojoh <johne12lao@gmail.com>
2026-06-15 22:00:06 -0700

    more fixy fixy

--- assets/Game.py
+++ assets/Game.py
@@ -180,7 +180,7 @@ class Game:
                 self.player.jump()
                 self.jump_times = 1
                 print("jump")
-            else:
+            elif not button1.is_pressed:
                 self.jump_times = 0
                 print("no")
             if self.button2.is_pressed:
