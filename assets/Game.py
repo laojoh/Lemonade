a4dfdaba3ae32f3da7d3fc94be5154c91bb66467
laojoh <johne12lao@gmail.com>
2026-06-15 18:35:40 -0700

    check 5

--- assets/Game.py
+++ assets/Game.py
@@ -206,7 +206,7 @@ class Game:
             rgba_bytes = pygame.image.tostring(scaled_surf, "RGBA")
             frame_array = np.frombuffer(rgba_bytes, dtype = np.uint8)
             frame_rect = frame_array.reshape(self.screen.get_height(), self.screen.get_width(), 4)
-            bgra_frame = frame_rect[:, :, [0, 1, 2, 3]].copy()
+            bgra_frame = frame_rect[:, :, [2, 3, 0, 1]].copy()
 
             self.fbmem.seek(0)
 
