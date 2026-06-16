7ca4cdaef53ffe0c673368c9c49e74a8042c68c9
laojoh <johne12lao@gmail.com>
2026-06-15 22:00:44 -0700

    oopsies pt.2

--- assets/Game.py
+++ assets/Game.py
@@ -180,7 +180,7 @@ class Game:
                 self.player.jump()
                 self.jump_times = 1
                 print("jump")
-            elif not button1.is_pressed:
+            elif not self.button1.is_pressed:
                 self.jump_times = 0
                 print("no")
             if self.button2.is_pressed:
