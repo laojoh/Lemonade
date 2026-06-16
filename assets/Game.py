a60c7b2830ddba017e5592199f7035a3fdfd1cba
laojoh <johne12lao@gmail.com>
2026-06-15 19:50:38 -0700

    check 21

--- assets/Game.py
+++ assets/Game.py
@@ -203,6 +203,18 @@ class Game:
 
             scaled_surf = pygame.transform.scale(self.shake_surface, self.screen.get_size())
 
+            pygame.draw.rect(
+                scaled_surf,
+                (0, 255, 0),
+                (0, 0, 100, 100)
+            )
+
+            pygame.draw.rect(
+                scaled_surf,
+                (255, 255, 255),
+                (100, 0, 100, 100)
+            )
+
             rgba_bytes = pygame.image.tostring(scaled_surf, "RGBA")
             frame_array = np.frombuffer(rgba_bytes, dtype = np.uint8)
             frame_rect = frame_array.reshape(self.screen.get_height(), self.screen.get_width(), 4)
