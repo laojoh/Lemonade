7716cbc6b8bf91e39110a846e882da2913f7b76f
laojoh <johne12lao@gmail.com>
2026-06-16 18:31:04 -0700

    trial 2 for color correcting

--- assets/scripts/utils.py
+++ assets/scripts/utils.py
@@ -4,14 +4,14 @@ import os
 BASE_IMG_PATH = "data/images"
 
 def load_image(path):
-    img = pygame.image.load(BASE_IMG_PATH + path).convert()
+    img = pygame.image.load(BASE_IMG_PATH + path).convert(32)
     img.set_colorkey((0, 0, 0))
     return img
 
 def load_images(path):
     images = []
     for img_name in sorted(os.listdir(str(BASE_IMG_PATH + path))):
-        images.append(load_image(path + "/" + img_name).convert())
+        images.append(load_image(path + "/" + img_name).convert(32))
     return images
 
 class Animation:
