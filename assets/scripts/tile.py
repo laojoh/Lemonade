3a44b2eda27c379c99b8f191535dc82f68a84ad2
laojoh <johne12lao@gmail.com>
2026-06-17 21:41:02 -0700

    color11

--- assets/scripts/tile.py
+++ assets/scripts/tile.py
@@ -17,6 +17,6 @@ class Tile:
 
     def render(self, surf, offset = (0, 0)):
         if (self.highlighted):
-            pygame.draw.rect(surf, (215, 215, 215), self.highlight_rect, border_radius=7)
+            pygame.draw.rect(surf, (217, 217, 217), self.highlight_rect, border_radius=7)
         
         pygame.draw.rect(surf, (210, 210, 210), self.rect, border_radius=5)
