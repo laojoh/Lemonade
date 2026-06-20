e65c2a91fabdeaac0788739d65a821c2bc5b3137
laojoh <johne12lao@gmail.com>
2026-06-19 17:47:36 -0700

    banner img test

--- assets/scripts/tile.py
+++ assets/scripts/tile.py
@@ -1,13 +1,14 @@
 import pygame
 
 class Tile:
-    def __init__(self, size, pos, num):
+    def __init__(self, size, pos, num,, banner_img):
         self.size = list(size)
         self.pos = list(pos)
         self.rect = pygame.Rect(pos[0], pos[1], size[0], size[1])
         self.highlight_rect = pygame.Rect(pos[0] - 2, pos[1] - 2, size[0] + 4, size[1] + 4)
         self.highlighted = False
         self.num = num
+        self.banner_img = banner_img
 
     def highlight(self, num):
         if self.num == num:
@@ -19,4 +20,4 @@ class Tile:
         if (self.highlighted):
             pygame.draw.rect(surf, (217, 217, 217), self.highlight_rect, border_radius=7)
         
-        pygame.draw.rect(surf, (210, 210, 210), self.rect, border_radius=5)
+        surf.blit(self.banner_img, self.rect)
